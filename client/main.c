c7a01699d9099ef9f1d040ef0130fc17090c0ae1
Simon Engledew <simon.engledew@osirium.com>
2017-02-06 00:02:50 +0000

    Remove unecessary imports

--- client/main.c
+++ client/main.c
@@ -1,12 +1,9 @@
 #include <arpa/inet.h>
 #include <netdb.h>
-// #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <sys/socket.h>
 #include <sys/stat.h>
-// #include <sys/types.h>
 #include <unistd.h>
 
 pid_t daemonize() {
